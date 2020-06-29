6970333d060d12a1757c45819ac1249081d67fd2
Salva Ruiz <salva@salvaruiz.me>
2020-06-29 23:07:49 +0200

    Fixed the race condition

--- exercise/problem/requests.go
+++ exercise/problem/requests.go
@@ -1,26 +1,40 @@
 package main
 
 import (
-	"io"
+	//"io"
 	"net/http"
-	"os"
+	//"os
+	"sync"
+	"fmt"
 )
 
 func main() {
-	sites := []string{
+	var wg sync.WaitGroup
+
+	sites := []string {
 		"https://www.google.com",
 		"https://drive.google.com",
-		"https://maps.google.com",
+		//"https://maps.google.com",
+		"https://500.google.com",
 		"https://hangouts.google.com",
 	}
 
-	for _, site := range sites {
-		go func(site string) {
-			res, err := http.Get(site)
-			if err != nil {
-			}
+	for index, site := range sites {
+		wg.Add(1)
 
-			io.WriteString(os.Stdout, res.Status+"\n")
-		}(site)
+		go func(index int, site string) {
+			defer wg.Done()
+
+			res, err := http.Get(site)
+
+			if err == nil {
+				//io.WriteString(os.Stdout, res.Status + "\n")
+				fmt.Println(index, "-", site, "->", res.Status)
+			} else {
+				fmt.Println(index, "-", site, "->", err)
+			}
+		}(index, site)
 	}
+
+	wg.Wait()
 }
